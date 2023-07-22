cd9b05a825496a0da2e73134ef390a9998dcbe46
Ezgi Efe <ezgiefee@gmail.com>
2023-07-23 00:02:56 +0300

    hello world!

---
+++ hello-world/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+	whatToSay := "Hello world, again!"
+	
+	sayHelloWorld(whatToSay)
+	
+}
+
+func sayHelloWorld(whatToSay string){
+	fmt.Println(whatToSay)
+}
+
